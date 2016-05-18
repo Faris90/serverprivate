0650c508c7d9f4eaefe5db8a95bc4100c0a7bf5e
Andrew54757 <andrews54757@gmail.com>
2016-05-18 13:51:06 -0400

    Update ConsoleService.js

--- src/core/ConsoleService.js
+++ src/core/ConsoleService.js
@@ -19,7 +19,7 @@ module.exports = class ConsoleService {
   start() {
     console.log('Starting ConsoleService');
     if (this.gameServer === undefined) {
-      throw "[ConsoleService] GameSever has not been set, cannot start!"
+      throw "[ConsoleService] [\x1b[31mFAIL\x1b[0m] GameSever has not been set, cannot start!"
     }
     if (!this.hasTitleBeenWriten) {
       this.writeTitle();
@@ -173,11 +173,11 @@ module.exports = class ConsoleService {
         execute(this.gameServer, split);
 
       } else {
-        console.warn('[ConsoleService] Failed to run command: ' + command + " args: " + split);
+        console.warn('[ConsoleService] [\x1b[31mFAIL\x1b[0m] Failed to run command: ' + command + " args: " + split);
       }
     }
     } catch (e) {
-      console.warn('[ConsoleService] Failed to run command: ' + command + " args: " + split);
+      console.warn('[ConsoleService] [\x1b[31mFAIL\x1b[0m] Failed to run command: ' + command + " args: " + split);
     }
 
   };
@@ -239,8 +239,8 @@ execommand(command, args) {
           try {
             self.parseCommands(str);
           } catch (err) {
-            console.log("[ERROR] Oh my, there seems to be an error with the command " + str);
-            console.log("[ERROR] Please alert AJS dev with this message:\n" + err);
+            console.log("[ERROR] [\x1b[31mFAIL\x1b[0m] Oh my, there seems to be an error with the command " + str);
+            console.log("[ERROR] [\x1b[31mFAIL\x1b[0m] Please alert AJS dev with this message:\n" + err);
           }
         } else {
           self.parseCommands(str); // dev mode, throw full error
