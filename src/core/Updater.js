aa245f58d1cb56a593565343337083005cb1e722
Andrew54757 <andrews54757@gmail.com>
2016-05-18 20:56:50 -0400

    Update Updater.js

--- src/core/Updater.js
+++ src/core/Updater.js
@@ -57,10 +57,10 @@ module.exports = class Updater {
 
   downloadFile(file, callback) {
     let url = this.url + file.src;
-    console.log('[Downloading] [\x1b[34mINFO\x1b[0m] ' + url + ' to: ' + file.dst + " \r ");
+    process.stdout.write('[Downloading] [\x1b[34mINFO\x1b[0m] ' + url + ' to: ' + file.dst + " \r ");
     request(url, function (error, response, body) {
       if (!error && response.statusCode == 200 && body != "") {
-        console.log("[Done] [\x1b[32mOK\x1b[0m] " + file.dst + " \r ")
+        process.stdout.write("\r[Done] [\x1b[32mOK\x1b[0m] " + file.dst)
         fs.writeFile(file.dst, body, (err, res)=> {
           if (typeof callback === "function") {
             callback(err, res);
